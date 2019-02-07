ba42992afd83f560b7dff43754fee822e3c334d4
unknown <discomushroom617@yahoo.com>
2019-02-06 18:24:01 -0600

    all done

--- django_ecommerce_html_forms/django_ecommerce_html_forms/urls.py
+++ django_ecommerce_html_forms/django_ecommerce_html_forms/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('create-product/', views.create_product, name='create_product'),
     path('edit-product/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete-product/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('toggle-featured/<int:product_id>/', views.toggle_featured, name='toggle_featureds'),
+    path('toggle-featured/<int:product_id>/', views.toggle_featured, name='toggle_featured'),
 ]
